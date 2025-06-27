4ebad2b561c21d1355e046d54cb28f4e76d440a1
SaiKumarReddy374 <saikumarreddypalakolanu6@gmail.com>
2025-06-27 16:04:55 +0530

    deployment ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,15 @@ const messageRoutes=require('./routes/message.route.js');
 const authRoutes=require('./routes/auth.route.js');
 const cors= require('cors');
 const {server, app } = require('./lib/socket.js');
+const path= require('path');
 
 dotenv.config();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
+
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -21,6 +24,12 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
+    }); 
+}
 app.get('/',(req,res)=>{
     res.send("API is running successully");
 });
