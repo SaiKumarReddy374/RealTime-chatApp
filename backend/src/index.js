7c7a8202c0554dc245ea01ff653c8c780634e2c7
SaiKumarReddy374 <saikumarreddypalakolanu6@gmail.com>
2025-06-27 18:15:04 +0530

    ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,41 @@
-const express=require('express');
-const dotenv=require('dotenv');
-const {connectDB} = require('./lib/db.js');
+const express = require('express');
+const dotenv = require('dotenv');
+const { connectDB } = require('./lib/db.js');
 const cookieParser = require('cookie-parser');
-const messageRoutes=require('./routes/message.route.js');
-const authRoutes=require('./routes/auth.route.js');
-const cors= require('cors');
-const {server, app } = require('./lib/socket.js');
-import { fileURLToPath } from 'url';
-import path from 'path';
+const messageRoutes = require('./routes/message.route.js');
+const authRoutes = require('./routes/auth.route.js');
+const cors = require('cors');
+const path = require('path');
+const { server, app } = require('./lib/socket.js');
 
-// This replaces __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
-
+// In CommonJS, __dirname works out of the box
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
-
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}))
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
-    }); 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
 }
-app.get('/',(req,res)=>{
-    res.send("API is running successully");
+
+app.get('/', (req, res) => {
+  res.send("API is running successfully");
 });
-const PORT =process.env.PORT || 3000;
-server.listen(PORT,()=>{
-    console.log('running on port: '+PORT);
-    connectDB();
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('Server running on port: ' + PORT);
+  connectDB();
 });
